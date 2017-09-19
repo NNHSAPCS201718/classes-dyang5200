b6dd296db9d0402fe9d8344847254f9c7b847f8c
dyang5200 <31215735+dyang5200@users.noreply.github.com>
2017-09-19 11:25:03 -0500

    Finished VendingMachine Lab

---
+++ Class Notes/VendingMachine.java
@@ -0,0 +1,51 @@
+
+/**
+ * Vending Machine
+ * 
+ * Methods:
+ *      - Insert Token
+ *      - Drop can
+ *      - Fill up with more cans
+ *      - Number of Tokens
+ *      - Number of Cans
+ */
+public class VendingMachine
+{
+    private int numTokens;
+    private int numCans;
+    
+    public VendingMachine()
+    {
+        this.numCans = 10;
+        this.numTokens = 0;
+    }
+    
+    public VendingMachine(int cans)
+    {
+        this.numCans = cans;
+        this.numTokens = 0;
+    }
+
+    public void fillUp(int cans)
+    {
+        this.numCans += cans;
+    }
+    
+    public void insertToken()
+    {
+        this.numCans -= 1;
+        this.numTokens += 1;
+    }
+    
+    public int getCanCount()
+    {
+        return this.numCans;
+    }
+    
+    public int getTokenCount()
+    {
+        return this.numTokens;
+    }
+    
+    
+    }
---
+++ Class Notes/VendingMachineTest.java
@@ -0,0 +1,78 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class VendingMachineTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class VendingMachineTest
+{
+    /**
+     * Default constructor for test class VendingMachineTest
+     */
+    public VendingMachineTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+    
+        @Test
+    public void testGetCanCount()
+    {
+        VendingMachine testVendingMachine = new VendingMachine();
+        int amount = testVendingMachine.getCanCount();
+        assertEquals(0,amount);
+    }
+    
+    @Test
+    public void testGetTokenCount()
+    {
+        VendingMachine testVendingMachine = new VendingMachine();
+        int amount = testVendingMachine.getTokenCount();
+        assertEquals(0, amount);
+    }
+    
+    @Test
+    public void testFillUp()
+    {
+        VendingMachine testVendingMachine = new VendingMachine();
+        testVendingMachine.fillUp(10);
+        int amount = testVendingMachine.getCanCount();
+        assertEquals(10,amount);
+        
+    }
+    
+    @Test
+    public void testInsertToken()
+    {
+        VendingMachine testVendingMachine = new VendingMachine();
+        testVendingMachine.insertToken();
+        int amount = testVendingMachine.getTokenCount();
+        assertEquals(1,amount);
+    }
+    
+}
